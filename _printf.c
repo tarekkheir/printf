e4c85d4ae1d4e96ed4b3f6b2e5eac5436afb5224
Tarek Kheir <kheirtarek@hotmail.fr>
2020-10-27 20:47:55 +0000

    printf

--- _printf.c
+++ _printf.c
@@ -13,7 +13,7 @@ int _printf(const char *format, ...)
 	char index[6] = {'c', 's', 'd', 'i', '%', '\0'};
 
 	va_start(my_list, format);
-	if (!format[i + 1] || format == NULL)
+	if (format == NULL || (format[0] == '%' && format[1] == 0))
 		return (-1);
 	while (format[i])
 	{
@@ -22,20 +22,18 @@ int _printf(const char *format, ...)
 			if (format[i + 1])
 			{
 				fi = find_index(format[i + 1], index);
-
 				if (fi != -1)
 				{
 					func = find_function(format[i + 1]);
 					result += func(my_list);
 					i++;
 				}
-			}
-			else
-			{
-
-				_putchar(format[i]);
-				_putchar(format[i + 1]);
-				result++;
+				else
+				{
+					_putchar(format[i]);
+					_putchar(format[i + 1]);
+					result++;
+				}
 			}
 		}
 		else
@@ -43,7 +41,6 @@ int _printf(const char *format, ...)
 			_putchar(format[i]);
 			result++;
 		}
-
 		i++;
 	}
 	va_end(my_list);
